9152107f8036c4c7a94759755106379fc13de7ef
saduni-udeshika <saduniudeshika0@gmail.com>
2022-06-01 17:50:50 +0530

    Fix Bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home/Home';
 import Chat from './pages/chat/Chat';
 import Nav from './components/nav/Nav';
+import Admin from './components/Admin/AdminHome';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} ></Route>
+          <Route exact path="/adminpanel" element={<Admin />} ></Route>
           <Route path="/chat" element={<Chat />} />
         </Routes>
       </Router>
