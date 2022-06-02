6736c8af8d03f1187ad428092384335b4d822e49
DhanukaDB <it20207236@my.sliit.lk>
2022-06-02 14:52:04 +0530

    studentSignup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Switch, Router, Route } from "react-router-dom";
 import Home from "./components/Home";
+import StudentSignup from './pages/Student/studentSignup';
 
 const App = () => {
   return (
 
    
-    <Router>  
-
-      <div>
-        <Routes>
+      <Router>  
+        <main> 
+         <Switch>
           <Route exact path="/" element={<Home />} ></Route>
-</Routes>
-</div>   
-</Router>
+          <Route exact path="/studentsignup" element={<StudentSignup />} ></Route>
+        </Switch>
+        </main>
+      </Router>
         
    
 
