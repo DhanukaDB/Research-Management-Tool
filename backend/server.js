ba5cc01ba6df15fbe876af14edf06b25baeedf7e
Sasini Perera <sasiniperera24@gmail.com>
2022-06-03 11:24:42 +0530

    update controllers and routes in evaluations

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require('path');
+const fileRoute = require('./routes/markinguploads')
 require("dotenv").config();
 
 const URL = process.env.MONGODB_URL;
@@ -12,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-const port = process.env.PORT || 5000;
 
 mongoose.connect(URL, {
   useNewUrlParser: true,
@@ -24,20 +25,22 @@ connection.once("open", () => {
   console.log("Mongodb connection success!");
 });
 
+app.use('/api/auth', require('./routes/authenticationRoutes'));
 
+// //Chat Router
+// app.use("/api/messages", require("./routes/messageRoutes"));
+
+
+//give feedback for the topics
 
 app.use("/api/sendFeedback",require ("./routes/topicEvaluateRoutes"));
 
-// const authRoutes = require("./routes/authenticationRoutes");
-// app.use("/auth", authRoutes);
 
 
-app.use('/api/auth', require('./routes/authenticationRoutes'));
-app.use('/api/student', require('./routes/studentRoutes'));
-app.use("/api/group", require("./routes/groupRoutes"));
+//Group Router
+// app.use("/api/group", require("./routes/groups"));
 
 //Evaluated docs Router
-
 app.use("/api/evaluateDocs", require("./routes/docsEvaluateRoutes"));
 
 //Evaluated presentations Router
@@ -49,16 +52,16 @@ app.use(fileRoute);
 
 app.get('*', (req, res) => {
    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-})
-const evaluatedDocRouter = require("./routes/EvaluatedDocs");
-app.use("/evaluateDocs", evaluatedDocRouter);
-//Evaluated Presentations Router
-const evaluatedPreRouter = require("./routes/EvaluatedPres");
-app.use("/EvaluatedPres", evaluatedPreRouter);
-//Evaluated Topics Router
-const evaluatedTopicsRouter = require("./routes/EvaluatedTopics");
-app.use("/EvaluateTopics", evaluatedTopicsRouter);
+});
 
-app.listen(port, (error) => {
-  console.log(`Server running on port ${port}`);
+
+// //Student Document Submission Router
+// const submissionRouter = require("./routes/submission");
+// app.use("/submission", submissionRouter);
+
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`server running on ${port}`)
 })
+
